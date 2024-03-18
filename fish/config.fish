94279615448bc3247a7f503545ff081f56e8f117
Danil Perzhul <perzhul.works@gmail.com>
2024-03-18 10:36:15 +0300

    feat: add work env variable

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
 alias vim=nvim
 alias ls="lsd -a"
+alias work="vim ~/work/geopie/"
 
 function last_history_item
     echo $history[1]
 end
 
 abbr -a !! --position anywhere --function last_history_item
-alias work="vim ~/work/geopie/"
+set -g -x work ~/work
