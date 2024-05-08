967e7e69c1293c657bf29b595220f741d85ff1ee
polukarp <perzhul.works@gmail.com>
2024-05-08 18:28:07 +0500

    chore: add some tweaks to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,30 @@
+set fish_greeting ""
+
 set -g -x work ~/work/
 set -g -x dotfiles ~/.dotfiles
 
-set fish_greeting
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
 
 alias vim=nvim
 alias work="vim ~/work/geopie/"
 alias todo="vim ~/personal/todo.md"
-alias fish="vim ~/.dotfiles/fish/config.fish"
+alias fishc="vim ~/.dotfiles/fish/config.fish"
 alias vimrc="vim ~/.dotfiles/nvim/init.vim"
 
 # Set the default editor to Neovim
 set -x EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
 
 alias ls="lsd --group-dirs first"
 alias l='ls -l'
